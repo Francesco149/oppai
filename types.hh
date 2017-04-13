4bbee15a2fff0ece0bc76d8175c7994e6e15b8df
Franc[e]sco <lolisamurai@tfwno.gf>
2017-04-13 19:57:43 +0200

    don't depend on stdint by default

--- types.hh
+++ types.hh
@@ -1,9 +1,18 @@
 #pragma once
 
-#include <stdint.h>
-
 typedef float   f32;
 typedef double  f64;
+
+#ifndef OPPAI_STDINT
+typedef unsigned int       u32;
+typedef unsigned short int u16;
+typedef unsigned char      u8;
+
+typedef int         i32;
+typedef short int   i16;
+typedef signed char i8;
+#else
+#include <stdint.h>
 
 typedef int_least32_t   i32;
 typedef int_least16_t   i16;
@@ -12,6 +21,7 @@ typedef int_least8_t    i8;
 typedef uint_least32_t  u32;
 typedef uint_least16_t  u16;
 typedef uint_least8_t   u8;
+#endif
 
 #define fi32 "d"
 #define fi16 "hd"
