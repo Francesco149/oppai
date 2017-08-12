9e347ab86728064ce1cd0e74254c8952cc850c31
Franc[e]sco <lolisamurai@tfwno.gf>
2017-08-12 04:22:38 +0200

    add packaging script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+
+git pull origin master
+
+echo -e "\nCompiling and Stripping"
+. "$dir"/build.sh -static || exit 1
+
+echo -e "\nPackaging"
+folder="oppai-$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)"
+mkdir -p "$folder"
+mv ./oppai $folder/oppai
+cp ./LICENSE $folder/LICENSE
+
+rm "$folder".tar.xz
+tar -cvJf "$folder".tar.xz \
+    "$folder"/oppai \
+    "$folder"/LICENSE
+
+echo -e "\nResult:"
+tar tf "$folder".tar.xz
+
+readelf --dynamic "$folder"/oppai
+ldd "$folder"/oppai
+
